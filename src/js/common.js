3fbe1e55e8b24a079032a08c1a101365176b0ee5
NazariyM <nazarriko@gmail.com>
2017-06-13 10:30:08 +0300

    build

--- src/js/common.js
+++ src/js/common.js
@@ -1,32 +1,19 @@
 $(function () {
 
+  $('.js-reserv-more').on('click', function () {
+    $('.reserv__more-in').fadeToggle();
+  });
+
   var $customSelect = $('.js-select');
-  $customSelect.selectric();
+  $customSelect.selectric({
+    disableOnMobile: false,
+    nativeOnMobile: false
+  });
 
   var $testimonialsSlider = $('.js-testimonials-slider');
   var $choiceSlider = $('.js-choice-slider');
 
-  $testimonialsSlider.on('afterChange init', function (event, slick, direction) {
-      console.log('afterChange/init', event, slick, slick.$slides);
-      // remove all prev/next
-      slick.$slides.removeClass('prevdiv').removeClass('nextdiv');
-
-      // find current slide
-      for (var i = 0; i < slick.$slides.length; i++) {
-        var $slide = $(slick.$slides[i]);
-        if ($slide.hasClass('slick-current')) {
-          // update DOM siblings
-          $slide.prev().addClass('slick-prev');
-          $slide.next().addClass('slick-next');
-          break;
-        }
-      }
-    }
-  ).on('beforeChange', function (event, slick) {
-    // optional, but cleaner maybe
-    // remove all prev/next
-    slick.$slides.removeClass('slick-prev').removeClass('slick-next');
-  }).slick({
+  $testimonialsSlider.slick({
     centerMode: true,
     focusOnSelect: true,
     useTransform: true,
@@ -58,12 +45,45 @@ $(function () {
     arrows: true,
     prevArrow: '<button type="button" class="choice-slider__btn choice-slider__btn_prev"><svg class="choice-slider__btn-icon icon-arr-sld_l"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="img/sprite.svg#icon-arr-sld_l"></use></svg></button>',
     nextArrow: '<button type="button" class="choice-slider__btn choice-slider__btn_next"><svg class="choice-slider__btn-icon icon-arr-sld_r"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="img/sprite.svg#icon-arr-sld_r"></use></svg></button>'
-    // responsive: [{
-    //   breakpoint: 767,
-    //   settings: {
-    //     centerMode: false,
-    //     centerPadding: 0
-    //   }
-    // }]
   });
+
+  (function () {
+    var $teamSLider = $('.js-team-slider');
+
+    function initSlider(slider) {
+      initSlickSlider(slider);
+      toggleSlider(1023, slider);
+      $(window).resize(function () {
+        toggleSlider(1023, slider);
+      });
+
+      function toggleSlider(breakpoint, slider) {
+        if ($(window).width() < breakpoint) {
+          if (!slider.hasClass('slick-initialized')) {
+            initSlickSlider(slider);
+          }
+        } else {
+          slider.slick('unslick');
+        }
+      }
+
+      function initSlickSlider(slider) {
+        slider.slick({
+          infinite: true,
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          prevArrow: '<button type="button" class="team__slider-btn team__slider-btn_prev"><svg class="team__slider-btn-icon icon-arr-sld_l"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="img/sprite.svg#icon-arr-sld_l"></use></svg></button>',
+          nextArrow: '<button type="button" class="team__slider-btn team__slider-btn_next"><svg class="team__slider-btn-icon icon-arr-sld_r"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="img/sprite.svg#icon-arr-sld_r"></use></svg></button>',
+          responsive: [{
+            breakpoint: 767,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+            }
+          }]
+        });
+      }
+    }
+    initSlider($teamSLider);
+  })();
 });
--- src/js/components/maps.js
+++ src/js/components/maps.js
@@ -1,68 +1,37 @@
-// $(function () {
-//
-// 	(function () {
-// 		ymaps.ready(init);
-// 		var excursionMap, locBalloon;
-//
-// 		function init() {
-// 			excursionMap = new ymaps.Map('js-excursion-map', {
-// 				center: [54.40504626991361,65.30454111414433],
-// 				zoom: 5
-// 			});
-//
-// 			// locBalloon = new ymaps.Balloon(myMap);
-// 			// locBalloon.options.setParent(myMap.options);
-// 			// myMap.balloon.open([53.19480611857074, 50.1113521604379], {
-// 			// 	contentBody: '<div class="location__balloon"><div class="location__balloon-logo"><img src="img/logo.svg"></div> <div class="location__balloon-text">Ленинская улица 168</div></div>'
-// 			// }, {
-// 			// 	closeButton: false,
-// 			// 	autoPan: true
-// 			// });
-//
-// 			excursionMap.behaviors.disable('scrollZoom');
-// 			excursionMap.controls.remove('zoomControl');
-// 			excursionMap.controls.remove('geolocationControl');
-// 			excursionMap.controls.remove('searchControl');
-// 			excursionMap.controls.remove('trafficControl');
-// 			excursionMap.controls.remove('typeSelector');
-// 			excursionMap.controls.remove('fullscreenControl');
-//
-// 		}
-// 	})();
-//
-// 	(function () {
-// 		ymaps.ready(init);
-// 		var aboutMap;
-//
-// 		function init() {
-// 			aboutMap = new ymaps.Map('js-about-map', {
-// 				center: [53.17558202121662,50.06785157719889],
-// 				zoom: 12
-// 			});
-//
-// 			// locBalloon = new ymaps.Balloon(myMap);
-// 			// locBalloon.options.setParent(myMap.options);
-// 			// myMap.balloon.open([53.19480611857074, 50.1113521604379], {
-// 			// 	contentBody: '<div class="location__balloon"><div class="location__balloon-logo"><img src="img/logo.svg"></div> <div class="location__balloon-text">Ленинская улица 168</div></div>'
-// 			// }, {
-// 			// 	closeButton: false,
-// 			// 	autoPan: true
-// 			// });
-//
-//
-// 			// [53.188681775199655,50.11000232293693]
-// 			// [53.187078071239924,50.07857949999998]
-// 			// [53.25022470164098,50.245655771163925]
-//
-// 			aboutMap.behaviors.disable('scrollZoom');
-// 			aboutMap.controls.remove('zoomControl');
-// 			aboutMap.controls.remove('geolocationControl');
-// 			aboutMap.controls.remove('searchControl');
-// 			aboutMap.controls.remove('trafficControl');
-// 			aboutMap.controls.remove('typeSelector');
-// 			aboutMap.controls.remove('fullscreenControl');
-//
-// 		}
-// 	})();
-//
-// });
+$(function () {
+	(function () {
+		ymaps.ready(init);
+		var aboutMap;
+
+		function init() {
+			aboutMap = new ymaps.Map('js-about-map', {
+				center: [53.17558202121662,50.06785157719889],
+				zoom: 12
+			});
+
+			// locBalloon = new ymaps.Balloon(myMap);
+			// locBalloon.options.setParent(myMap.options);
+			// myMap.balloon.open([53.19480611857074, 50.1113521604379], {
+			// 	contentBody: '<div class="location__balloon"><div class="location__balloon-logo"><img src="img/logo.svg"></div> <div class="location__balloon-text">Ленинская улица 168</div></div>'
+			// }, {
+			// 	closeButton: false,
+			// 	autoPan: true
+			// });
+
+
+			// [53.188681775199655,50.11000232293693]
+			// [53.187078071239924,50.07857949999998]
+			// [53.25022470164098,50.245655771163925]
+
+			aboutMap.behaviors.disable('scrollZoom');
+			aboutMap.controls.remove('zoomControl');
+			aboutMap.controls.remove('geolocationControl');
+			aboutMap.controls.remove('searchControl');
+			aboutMap.controls.remove('trafficControl');
+			aboutMap.controls.remove('typeSelector');
+			aboutMap.controls.remove('fullscreenControl');
+
+		}
+	})();
+
+});
